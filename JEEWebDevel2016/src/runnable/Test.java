e2003889cc64f27d6dd0b1f4c6a9ef91a01c7da9
adi <adi@BLOOM3.bdi2015.local>
2016-08-03 11:40:35 +0200

    Lambda testing

--- JEEWebDevel2016/src/runnable/Test.java
+++ JEEWebDevel2016/src/runnable/Test.java
@@ -1,5 +1,10 @@
 package runnable;
 
+import java.util.Arrays;
+import java.util.Collections;
+import java.util.Comparator;
+import java.util.List;
+
 public class Test {
 
 	public static void main(String[] args) {
@@ -10,6 +15,26 @@ public class Test {
 		 * test class change
 		 */
 
+		List<String> list = Arrays.asList("warsaw", "london", "paris", "madrid");
+		
+		/*
+		list.sort(new Comparator<String>() {
+
+			@Override
+			public int compare(String o1, String o2) {
+				// TODO Auto-generated method stub
+				return o1.compareTo(o2);
+			}
+			
+		});
+		*/
+		
+		Collections.sort(list, (String o1, String o2) -> o1.compareTo(o2));
+		
+		
+		list.forEach(System.out::println);
+		
+		
 	}
 
 }
